b2b6694e289cfad23fdce03ad2b8354777f653b6
William Husum <husum44@gmail.com>
2021-12-06 19:36:33 +0100

    Initial commit

---
+++ src/com/AOC/dayTwo/Main.java
@@ -0,0 +1,10 @@
+package com.AOC.dayTwo;
+
+import static com.AOC.dayTwo.Submarine.CalcPosition;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CalcPosition();
+    }
+}
